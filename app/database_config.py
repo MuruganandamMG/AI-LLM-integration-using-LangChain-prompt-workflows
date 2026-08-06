c733882a36d14fb3ab4b5d770525596df9b2704a
MuruganandamMG <muruga004kvp@gmail.com>
2026-08-06 12:42:38 +0000

    perf(db): configure SQLite Write-Ahead Logging (WAL) and synchronous pragma optimizations

---
+++ app/database_config.py
@@ -0,0 +1,9 @@
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA synchronous=NORMAL")
+    cursor.close()
